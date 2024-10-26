ab375779750241adc490ccfd49176e928a413539
mike <s76354m@gmail.com>
2024-10-25 22:02:26 -0700

    Cursor

--- Axis/Models/Project.cs
+++ Axis/Models/Project.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Numerics;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProgramManagement.Models;
 
@@ -13,19 +14,55 @@ public class Project
         Notes = new HashSet<ProjectNote>();
     }
 
+    [Required]
+    [StringLength(50)]
+    [Display(Name = "Project ID")]
     public string ProjectId { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    [Display(Name = "Project Type")]
     public string ProjectType { get; set; }
+
+    [StringLength(500)]
+    [Display(Name = "Description")]
     public string ProjectDescription { get; set; }
+
+    [StringLength(100)]
+    [Display(Name = "Project Manager")]
     public string ProjectManager { get; set; }
+
+    [StringLength(100)]
     public string Analyst { get; set; }
+
+    [StringLength(100)]
+    [Display(Name = "Benchmark File ID")]
     public string BenchmarkFileId { get; set; }
+
+    [Required]
+    [Display(Name = "Go Live Date")]
+    [DataType(DataType.Date)]
     public DateTime GoLiveDate { get; set; }
+
     public DateTime LastEditDate { get; set; }
+    
+    [StringLength(100)]
     public string LastEditMSID { get; set; }
+
+    [Display(Name = "Mileage")]
     public int Mileage { get; set; }
+
     public string NDBLOB { get; set; }
+
+    [Required]
+    [StringLength(50)]
     public string Status { get; set; }
+
+    [StringLength(100)]
+    [Display(Name = "New Market")]
     public string NewMarket { get; set; }
+
+    [Display(Name = "Refresh Indicator")]
     public bool RefreshInd { get; set; }
 
     // Navigation properties
